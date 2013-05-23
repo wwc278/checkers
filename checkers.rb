1c1208e854139c5601ddf7f53d6fc68ac0f67a9c
App Academy Student <student@appacademy.io>
2013-05-23 09:47:13 -0700

    Initial commit. Add checkers, board, and pieces (.rb) files as well as colorize.rb.

---
+++ checkers.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+require './colorize'
+require './board'
+require './pieces'
+
+class Checkers
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __FILE__ == $PROGRAM_NAME
+  newgame = Checkers.new
+  newgame.play
+end
